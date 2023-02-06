afcf2d26ef00c639d1adc84b98731e9ed272a7ef
aeronalmira0005 <69041811+aeronalmira0005@users.noreply.github.com>
2023-02-06 20:33:19 +0800

    Update streamlit_app.py
    Added multiselect for Own Fruit Smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,5 +13,8 @@ streamlit.text('🥑🍞Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+fruit_list = fruit_list.set_index('Fruit')
+
+streamlit.multiselect('Pick some fruits:', list(fruit_list.index))
 
 streamlit.dataframe(fruit_list)
