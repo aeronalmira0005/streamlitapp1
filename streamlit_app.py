c85a2e2cbf0ee88edb537db1625f29a53e1841a3
aeronalmira0005 <69041811+aeronalmira0005@users.noreply.github.com>
2023-02-06 20:57:25 +0800

    Update streamlit_app.py
    Added filter for fruits table in Own Fruit Smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,7 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruit_list = fruit_list.set_index('Fruit')
 
-streamlit.multiselect('Pick some fruits:', list(fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect('Pick some fruits:', list(fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(fruit_list)
+streamlit.dataframe(fruits_to_show)
