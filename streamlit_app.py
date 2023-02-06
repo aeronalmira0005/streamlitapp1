9a229edf433fe430c4fb7c7c6a59688d3aac7ffb
aeronalmira0005 <69041811+aeronalmira0005@users.noreply.github.com>
2023-02-06 20:48:44 +0800

    Update streamlit_app.py
    Fix1 for wrong preselected fruit index for Own Fruit Smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,6 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 fruit_list = fruit_list.set_index('Fruit')
 
-streamlit.multiselect('Pick some fruits:', list(fruit_list.index), ['Avocado', 'Strawberry'])
+streamlit.multiselect('Pick some fruits:', list(fruit_list.index), ['Avocado', 'Strawberries'])
 
 streamlit.dataframe(fruit_list)
