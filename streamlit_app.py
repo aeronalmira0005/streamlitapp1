134e35a7ec1ecac7e8346d51efdc5dbffd9b93a9
aeronalmira0005 <69041811+aeronalmira0005@users.noreply.github.com>
2023-02-06 20:17:05 +0800

    Update streamlit_app.py
    Added Breakfast Menu and Own Fruit Smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 
 import streamlit
 
-streamlit.title('Title Sample')
-streamlit.header('Header Sample')
-streamlit.text('Text Sample')
+streamlit.title('My Parents New Healthy Diner')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
